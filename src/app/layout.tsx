a32777cd3e9ff703aaa13159d673f4a51da37633
andre yvan laurel kanmegne tabouguie <andreyvanlaurelkanmegnetabouguie@andres-MacBook-Air.local>
2025-04-19 13:04:19 +0200

    🚫 Remove animate.css CDN import from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
         />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
+      
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
           defer
